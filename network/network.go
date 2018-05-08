7e3a37d54c113519ebc64688e294f374f64ddeee
Clinton Reeder <clinton_reeder@byu.edu>
2018-05-08 12:07:10 -0600

    JSON updates

--- network/network.go
+++ network/network.go
@@ -1,83 +1,96 @@
 package network
 
 import (
-        "fmt"
-        "log"
-        "time"
+	"fmt"
+	"log"
+	"time"
 
-        "github.com/byuoitav/event-router-microservice/base"
-        "github.com/fatih/color"
-        "github.com/gorilla/websocket"
+	"github.com/byuoitav/event-router-microservice/base"
+	"github.com/fatih/color"
+	"github.com/gorilla/websocket"
 )
 
 const (
-        // Time allowed to write a message to the peer.
-        writeWait = 10 * time.Second
+	// Time allowed to write a message to the peer.
+	writeWait = 10 * time.Second
 
-        // Time allowed to read the next pong message from the router.
-        pingWait = 90 * time.Second
+	// Time allowed to read the next pong message from the router.
+	pingWait = 90 * time.Second
 
-        // Interval to wait between retry attempts
-        retryInterval = 3 * time.Second
+	// Interval to wait between retry attempts
+	retryInterval = 3 * time.Second
 )
 
 var upgrader = websocket.Upgrader{
-	ReadBufferSize: 1024,
+	ReadBufferSize:  1024,
 	WriteBufferSize: 1024,
 }
 
-type command struct {
-	username []string
-	password []string
-	addrInput []string
-	addrVOutput []string
-	addrAOutput []string
+type Creds struct {
+	Username string `json:"username"`
+	Password string `json:"password"`
 }
-/* Left over from previous code
-type Node struct {
-        Name          string
-        Conn          *websocket.Conn
-        WriteQueue    chan base.Message
-        ReadQueue     chan base.Message
-        DecoderAddress string
-        filters       map[string]bool
-        readDone      chan bool
-        writeDone     chan bool
-        lastPingTime  time.Time
-        state         string
+
+type SwitchConfigSet struct {
+	Name   string   `json:"name"`
+	Config []Config `json:"config"`
 }
-*/
-//func (n *Node) OpenConnection() error {
+
+type Config struct {
+	Multicast `json:"multicast"`
+	Name      string `json:"name"`
+}
+
+type Multicast struct {
+	Address string `json:"address"`
+}
+
+type Command struct {
+	Creds
+	SwitchConfigSet `json:"config_set"`
+}
+
 func OpenConnection() error {
-        //var mess = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.1"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.1"},"name": "ip_input3"}]}}`)
-
-        var m = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.2"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.2"},"name": "ip_input3"}]}}`)
-
-        //var mess = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.11"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.11"},"name": "ip_input3"}]}}`)
-	//open connection to the decoder
-        dialer := &websocket.Dialer{
-                HandshakeTimeout: 10 * time.Second,
-        }
-
-        //conn, _, err := dialer.Dial(fmt.Sprintf("ws://%s/wsapp", n.DecoderAddress), nil)
-        conn, test, err := dialer.Dial(fmt.Sprintf("ws://192.168.0.7/wsapp/"), nil)
+	//var mess = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.1"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.1"},"name": "ip_input3"}]}}`)
+	//var m = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.2"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.2"},"name": "ip_input3"}]}}`)
+	//var mess = []byte(`{"username":"admin","password":"Atlona","config_set":{"name":"ip_input","config":[{"multicast": {"address": "239.1.1.11"},"name": "ip_input1"},{"multicast": {"address": "239.10.1.11"},"name": "ip_input3"}]}}`)
+	less := []Config{Config{Multicast: Multicast{Address: "239.0.0.1"}, Name: "ip_input1"}, Config{Multicast: Multicast{Address: "239.10.0.1"}, Name: "ip_input3"}}
+	Test := Command{Creds: Creds{Username: "admin", Password: "password"}, SwitchConfigSet: SwitchConfigSet{Name: "ip_input", Config: less}}
+	fmt.Println(Test)
+	Foo, err := json.Marshal(Test)
 	if err != nil {
-                log.Printf(color.HiRedString("There was a problem establishing the websocket with 192.168.0.7: %v", err.Error()))
-                return err
-        }
-        //n.Conn = conn
-        if conn != nil {
+		fmt.Println("Error:", err)
+	}
+	//Bar, err := json.Marshal(Command{Creds: Creds{Username: "admin", Password: "password"}, ConfigSet: ConfigSet{Name: "ip_table", Config: Config{Multicast: Multicast{Address:"239.0.0.1"}}}})
+	if err != nil {
+		fmt.Println("Error:", err)
+	}
+	m := string(Foo)
+
+	//open connection to the decoder
+	dialer := &websocket.Dialer{
+		HandshakeTimeout: 10 * time.Second,
+	}
+
+	conn, test, err := dialer.Dial(fmt.Sprintf("ws://192.168.0.7/wsapp/"), nil)
+	if err != nil {
+		log.Printf(color.HiRedString("There was a problem establishing the websocket with 192.168.0.7: %v", err.Error()))
+		return err
+	}
+	if conn != nil {
 		fmt.Println(conn)
 		fmt.Println(test)
 	}
 	err = conn.WriteMessage(websocket.TextMessage, m)
 	if err != nil {
 		log.Println(err)
-		return(err)
+		return (err)
 	}
-	// Need to read the message after I write a message and pip it out so that it can be logged
-	// Build a error capturing setup based upon conn.ReadMessage -> dump message to variable and check
-
+	_, msg, err := conn.ReadMessage()
+	if err != nil {
+		log.Println(err)
+	}
+	fmt.Println(m)
 	conn.Close()
-        return nil
+	return nil
 }
